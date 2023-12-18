9be102cd332bb18084ff22db5571deeb264b18b6
kaylonm17 <you@example.com>
2023-12-17 22:34:06 -0600

    New Profile Form

--- src/scenes/global/sidebar.jsx
+++ src/scenes/global/sidebar.jsx
@@ -133,6 +133,7 @@ const Sidebar = () => {
                             selected={selected}
                             setSelected={setSelected}
                             />
+                            <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px"}}>Pages</Typography>
                             <Item
                             title="Profile Form"
                             to="/form"
@@ -154,6 +155,7 @@ const Sidebar = () => {
                             selected={selected}
                             setSelected={setSelected}
                             />
+                            <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px"}}>Charts</Typography>
                             <Item
                             title="Bar Chart"
                             to="/bar"
